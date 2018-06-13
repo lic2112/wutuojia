3aa354ed36870bae41c9ae006b66401c6a5091fa
lic2112 <ttxx.bnb1@gmail.com>
2018-06-13 20:11:33 +0800

    list es6 start

--- js/list.js
+++ js/list.js
@@ -1,40 +1,58 @@
 // recomd数据请求
-$.ajax({
-    url: 'http://127.0.0.1/wutuojia/php/data.php',
-    type: 'post',
-    data: { recommd: 1 },
-    dataType: 'json',
-    success: function (res) {
-        // console.log(res)
-        for (let i = 0; i < res.length; i++) {
-            // $('.recommd-main').find('.lazyload').eq(i).attr('data-src', res[i].data_src)
-            $('.recommd-body').eq(i).find('.lazyload').attr('data-src', res[i].data_src)
-            $('.recommd-body').eq(i).find('dd').find('a').html(res[i].goods)
-            $('.recommd-body').eq(i).find('dd').find('p').html(res[i].info1)
-            $('.recommd-body').eq(i).find('dd').find('span').html(res[i].info2)
-        }
-    },
-    beforeSend: function () {
-        let str = '';
-        for (let i = 0; i < 12; i++) {
-            str += `<div class='recommd-body'>
-                        <dl>
-                            <dt>
-                                <a href='javascript:'>
-                                    <img src='img/rd1.png' alt='' class='lazyload'>
-                                </a>
-                            </dt>
-                            <dd>
-                                <a href='javascript:'></a>
-                                <p></p>
-                                <span></span>
-                            </dd>
-                        </dl>
-                    </div>`
-        }
-        $('.recommd-main').html(str)
-    }
-});
+// $.ajax({
+//     url: 'http://127.0.0.1/wutuojia/php/data.php',
+//     type: 'post',
+//     data: { recommd: 1 },
+//     dataType: 'json',
+//     success: function (res) {
+//         // console.log(res)
+//         // for (let i = 0; i < res.length; i++) {
+//         //     // $('.recommd-main').find('.lazyload').eq(i).attr('data-src', res[i].data_src)
+//         //     $('.recommd-body').eq(i).find('.lazyload').attr('data-src', res[i].data_src)
+//         //     $('.recommd-body').eq(i).find('dd').find('a').html(res[i].goods)
+//         //     $('.recommd-body').eq(i).find('dd').find('p').html(res[i].info1)
+//         //     $('.recommd-body').eq(i).find('dd').find('span').html(res[i].info2)
+//         // }
+//         // let str = '';
+//         // for (let i = 0; i < 12; i++) {
+//         //     str += `<div class='recommd-body'>
+//         //                 <dl>
+//         //                     <dt>
+//         //                         <a href='javascript:'>
+//         //                             <img src='img/rd1.png' alt='' data-src='${res[i].data_src}' class='lazyload'>
+//         //                         </a>
+//         //                     </dt>
+//         //                     <dd>
+//         //                         <a href='javascript:'>${res[i].goods}</a>
+//         //                         <p>${res[i].info1}</p>
+//         //                         <span>${res[i].info1}</span>
+//         //                     </dd>
+//         //                 </dl>
+//         //             </div>`
+//         // }
+//         // $('.recommd-main').html(str)
+//     },
+//     beforeSend: function () {
+//         //     let str = '';
+//         //     for (let i = 0; i < 12; i++) {
+//         //         str += `<div class='recommd-body'>
+//         //                     <dl>
+//         //                         <dt>
+//         //                             <a href='javascript:'>
+//         //                                 <img src='img/rd1.png' alt='' data-src='${res[i].data_src}' class='lazyload'>
+//         //                             </a>
+//         //                         </dt>
+//         //                         <dd>
+//         //                             <a href='javascript:'>${res[i].goods}</a>
+//         //                             <p>${res[i].info1}</p>
+//         //                             <span>${res[i].info1}</span>
+//         //                         </dd>
+//         //                     </dl>
+//         //                 </div>`
+//         //     }
+//         //     $('.recommd-main').html(str)
+//         // }
+//     });
 
 // 分页
 // $('.pagination').pagination(res.length, {
@@ -45,21 +63,61 @@ $.ajax({
 //         updateData(index, res)
 //     }
 // })
-$('.pagination').pagination(10, {
+
+class Page {
+    constructor() {
+
+        this.askData()
+    }
+    askData() {
+        let that = this;
+        $.ajax({
+            url: 'http://127.0.0.1/wutuojia/php/data.php',
+            type: 'post',
+            data: { recommd: 1 },
+            dataType: 'json',
+            success: function (res) {
+                that.mypagination(res)
+                // let str = '';
+                // for (let i = 0; i < 12; i++) {
+                //     str += `<div class='recommd-body'>
+                //                 <dl>
+                //                     <dt>
+                //                         <a href='javascript:'>
+                //                             <img src='img/rd1.png' alt='' data-src='${res[i].data_src}' class='lazyload'>
+                //                         </a>
+                //                     </dt>
+                //                     <dd>
+                //                         <a href='javascript:'>${res[i].goods}</a>
+                //                         <p>${res[i].info1}</p>
+                //                         <span>${res[i].info1}</span>
+                //                     </dd>
+                //                 </dl>
+                //             </div>`
+                // }
+                // $('.recommd-main').html(str)
+            }
+        });
+    }
+    mypagination(res) {
+        // console.log(res);
+        $('.pagination').pagination(res.length, {
     num_edge_entries: 2,         //两侧显示的首尾分页的条目数
     num_display_entries: 4,      //连续分页主体部分显示的分页条目数
-    callback: function (index) {
+            callback: function (index, res) {       //index为当前页索引
+                console.log(index);
+                console.log(res);
         //alert(index)
-        console.log(1);
+                updateData(index, res)
     },
-    items_per_page: 1,          //每页显示的条目数
+            items_per_page: 16,          //每页显示的条目数
     prev_text: '上一页',
-    next_text: '下一页',
+            next_text: '下一页'
 
 });
-
-
-
+    }
+}
+new Page()
 
 
 //懒加载
